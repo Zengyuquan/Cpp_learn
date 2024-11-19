3ab2a004a4b3fd22a2feec0dd3ad193d0a33f0d1
Zengyuquan <95689130+Zengyuquan@users.noreply.github.com>
2024-11-19 15:15:40 +0800

    11/19

--- Demo/Objection/24_对象特征-函数的分类以及调用.cpp
+++ Demo/Objection/24_对象特征-函数的分类以及调用.cpp
@@ -31,16 +31,28 @@ public:
 void test01()
 {
     // 1、括号法
-    Person p1;              // 默认构造函数调用
-    Person p2(10);       // 有参构造函数
-    Person p3(p2);          // 拷贝构造函数
+    //Person p1;              // 默认构造函数调用
+    //Person p2(10);       // 有参构造函数
+    //Person p3(p2);          // 拷贝构造函数
 
     // 注意事项1
     // 默认调用构造函数时候，不要加()
     // 因为下面这段代码，编译器会认为是一个函数的声明，不会认为是在创建对象
 //    Person p1();
-    cout << "p2的年龄为：" << p2.age << endl;
-    cout << "p3的年龄为：" << p3.age << endl;
+//    cout << "p2的年龄为：" << p2.age << endl;
+//    cout << "p3的年龄为：" << p3.age << endl;
+
+    // 2、显示法
+    Person p1;
+    Person p2 = Person(10);     // 有参构造
+    Person p3 = Person(p2);        // 拷贝构造
+
+    Person(10);     // 匿名对象 特点：当前行执行结束后，系统会立即回收掉匿名对象
+    cout << "aaa" << endl;
+
+    // 注意事项
+    // 不要利用拷贝构造函数   初始化匿名对象  编译器会认为Person (p3) === Person p3;
+    // Person(p3);
 }
 
 int main()
